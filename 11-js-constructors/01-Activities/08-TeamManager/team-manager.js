02c94b0826273ed4edec514bc0d8318bfe1af61d
bruce2tr <bruce.trammell@gmail.com>
2019-07-05 18:48:13 -0400

    prior work added

---
+++ 11-js-constructors/01-Activities/08-TeamManager/team-manager.js
@@ -0,0 +1,17 @@
+var inquirer = require("inquirer");
+
+function Player(name, position, offense, defense){
+    this.name = name;
+    this.position = position;  
+    this.offense = offense; 
+    this.defense = defense;
+
+    this.printStats = function(){
+        console.log("Name: " + this.name);
+        console.log("Position: " + this.position);
+        console.log("Offense: " + this.offense);
+        console.log("Defense: " + this.defense);
+    }
+
+}
+
